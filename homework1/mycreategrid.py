5aa39bff9c8273abede6a9130757a15c88e8f18e
Kristian Suhartono <blackxephos2@gmail.com>
2018-01-12 21:39:56 -0500

    A super rough solution by drawing boxes around obstacles

--- homework1/mycreategrid.py
+++ homework1/mycreategrid.py
@@ -28,7 +28,24 @@ def myCreateGrid(world, cellsize):
 	grid = None
 	dimensions = (0, 0)
 	### YOUR CODE GOES BELOW HERE ###
+        dimensions = (int(math.ceil(world.getDimensions()[0]/cellsize)), int(math.ceil(world.getDimensions()[1]/cellsize)))
+        grid = [[True]*dimensions[1] for _ in range(dimensions[0])]
+
+        for obstacle in world.getObstacles():
+            minX = obstacle.getPoints()[0][0]
+            minY = obstacle.getPoints()[0][1]
+            maxX = obstacle.getPoints()[0][0]
+            maxY = obstacle.getPoints()[0][1]
+            for i in range(1, len(obstacle.getPoints())):
+                minX = obstacle.getPoints()[i][0] if obstacle.getPoints()[i][0] < minX else minX
+                maxX = obstacle.getPoints()[i][0] if obstacle.getPoints()[i][0] > maxX else maxX
+                minY = obstacle.getPoints()[i][1] if obstacle.getPoints()[i][1] < minY else minY
+                maxY = obstacle.getPoints()[i][1] if obstacle.getPoints()[i][1] > maxY else maxY
+
+            for i in range(int(minX-0.5), int(maxX+0.5)):
+                for j in range(int(minY-0.5), int(maxY+0.5)):
+                    if grid[int(i/cellsize)][int(j/cellsize)]:
+                        grid[int(i/cellsize)][int(j/cellsize)] = False
 
 	### YOUR CODE GOES ABOVE HERE ###
 	return grid, dimensions
-
